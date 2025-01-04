c11e60c250813b9d4e645fd2e3d99dcb64c71b01
Alexsandro Oliveira <alexsandro.sfo@gmail.com>
2025-01-03 21:57:13 -0300

    added extenal pom.xml with module

---
+++ anime-service/src/main/java/com/alex/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.alex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hi")
+    public String hello() {
+        return "OMAE WA MOU SHINDE IRU";
+    }
+}
